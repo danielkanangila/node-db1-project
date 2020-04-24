3755c9b37d24fdda5ae392ea94b1008c5d9bf997
Daniel Kanangila <daniel.kanangila@gmail.com>
2020-04-24 19:45:33 -0400

    created accounts API endpoints

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const accountRouter = require("./accounts/accountRouter");
+const cors = require("cors");
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+server.use(cors());
+server.use("/api", accountRouter);
 
 module.exports = server;
